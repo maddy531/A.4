600f6faff5cb17647cdb6d797d7cdc61ccc1dcd8
Maddy <maddy531@gmail.com>
2015-11-20 14:20:33 -0500

    initial commit

---
+++ assessment4.js
@@ -0,0 +1,17 @@
+( function(){
+
+var animals = [
+	{ name:’Dogs’, sound: ‘arf’, },
+	{name:’Cats’, sound: ‘meow’},
+	{name:’ Monkey’, sound:’man ahh ahh ’}
+	];
+
+function seeAndSay(){
+	animals.forEach( function(animal){
+  animals.gray = gray;
+	console.log( ‘The ’ + animal.name + ‘ says ’ animal.sound + ‘! ’);
+
+})
+};
+
+})( );
