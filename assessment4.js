5447ef3a2d7dc1c11f4e6b9d5ded5df05c52df99
Maddy <maddy531@gmail.com>
2015-11-20 15:11:41 -0500

    Corrected in class

--- assessment4.js
+++ assessment4.js
@@ -1,17 +1,18 @@
-( function(){
+(function(){
 
 var animals = [
-	{ name:’Dogs’, sound: ‘arf’, },
-	{name:’Cats’, sound: ‘meow’},
-	{name:’ Monkey’, sound:’man ahh ahh ’}
+	{name:'Dog', sound: 'arf'},
+	{name:'Cat', sound: 'meow'},
+	{name:'Monkey', sound:'man ahh ahh'}
 	];
 
 function seeAndSay(){
-	animals.forEach( function(animal){
-  animals.gray = gray;
-	console.log( ‘The ’ + animal.name + ‘ says ’ animal.sound + ‘! ’);
+	animals.forEach(function(animal){
+    animal.color = 'gray';
+	console.log( 'The ' + animal.name + ' says ' + animal.sound + '! And is' + animal.color);
 
-})
-};
+});
+}
+  seeAndSay();
 
-})( );
+})();
